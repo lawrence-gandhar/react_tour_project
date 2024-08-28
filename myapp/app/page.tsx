d27142c78effcbd7411f0e90c0c9ba9fca84ddbb
Lawrence Gandhar <lawrence_gandhar@yahoo.com>
2024-08-28 22:16:52 +0530

    updated

--- myapp/app/page.tsx
+++ myapp/app/page.tsx
@@ -27,6 +27,10 @@ export default function Home() {
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                 <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
             </div>
+            <div className="py-2 px-4">
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+            </div>
             <div className="flex items-center justify-between py-2 px-4">
                 <div className="flex items-start">
                     <div className="flex items-center h-5">
@@ -40,6 +44,7 @@ export default function Home() {
             </div>
             <div className="py-2 px-4">
               <button type="submit" className="w-full btn-primary py-1 rounded">Sign in</button>
+              <button type="submit" className="w-full btn-primary py-1 rounded">Sign Up</button>
             </div>
             <div className="py-2 px-4 pb-4 text-center">
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
