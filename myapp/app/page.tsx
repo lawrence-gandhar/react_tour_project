57d02bc0b4dafd0226a1ef89d05fbbac2df08606
Lawrence Gandhar <lawrence_gandhar@yahoo.com>
2024-08-29 16:51:09 +0530

    updated login page with JWT

--- myapp/app/page.tsx
+++ myapp/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import '../styles/login_page.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
 
@@ -9,27 +9,32 @@ export default function Home() {
   const [username, setUsername] = useState("")
   const [passwd, setPassword] = useState("")
   const [confirm_passwd, setConfirmPassword] = useState("")
+  const BACKEND_SERVER = "http://localhost:8000/"
+
+  const [authorized, setAuthorized] = useState(-1)
 
   const signin = async () => {
-    alert("clicked")
-    console.log(username, passwd)
+    console.log(username, passwd, BACKEND_SERVER)
 
-    // let response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    //     method: 'POST',
-    //     body: JSON.stringify({
-    //         username: username,
-    //         passwd: passwd
-    //     }),
-    //     headers: {
-    //         'Content-type': 'application/json'
-    //     }
-    // })
-
-    // response = await response.json()
-
-    // alert(JSON.stringify(response))
+    let response = await fetch(BACKEND_SERVER+'api/token', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: username,
+            password: passwd
+        }),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }).then(response=>{
+      if(response.status===401){
+        setAuthorized(0)
+        console.log(authorized)
+      }else if(response.status===200){
+        setAuthorized(1)
+      }
+    })
   }
-
+  
 
   const signup = async () => {
     if (passwd !== confirm_passwd){
@@ -48,10 +53,6 @@ export default function Home() {
     }
   }
 
-
-
-
-
   return (
     <div className="backgroundImage h-screen">
       <div className="flex flex-row min-h-screen justify-center items-center">
@@ -66,7 +67,8 @@ export default function Home() {
           <form className="col-span-12 login-div" action="#">
             <div className="py-2 px-4 pt-4">
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input value={username} onChange={e => {setUsername(e.target.value)}} type="email" name="email" id="email" 
+                <input value={username} onChange={e => {setUsername(e.target.value)}} type="text" name="username" 
+                id="username" 
                 className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg 
                 focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700
                   dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
@@ -82,6 +84,7 @@ export default function Home() {
                 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
                 dark:focus:border-blue-500" required/>
             </div>
+
             {action==="Sign Up"?
               <div className="py-2 px-4">
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
@@ -94,6 +97,7 @@ export default function Home() {
                       dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
               </div>:<div></div>
             }
+
             {action==="Login"?
               <div className="flex items-center justify-between py-2 px-4">
                   <div className="flex items-start">
@@ -107,6 +111,7 @@ export default function Home() {
                   <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
               </div>:<div></div>
             }
+            
             <div className="py-2 px-4">
               {
                 action==="Sign Up"?
@@ -114,18 +119,43 @@ export default function Home() {
                 <button type="button" className="w-full btn-primary py-1 rounded" onClick={signin}>Sign in</button>
               }              
             </div>
+              
+            {
+              authorized == 0 && action==="Login" ? 
+                <div className="col-span-12 px-4 py-2 ">
+                <p className="w-full error-div px-2 py-2">
+                  Not a valid username or password. Please check the username and password.
+                </p></div>:<div></div>
+
+            }
+
             {
                 action==="Sign Up"?
                 <div className="py-2 px-4 pb-4 text-center">
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Have an account ? 
-                      <a href="#" onClick={()=>{setAction("Login")}} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</a>
+                      <a href="#" onClick={()=>{
+                          setAction("Login"); 
+                          setAuthorized(-1)
+                          setUsername(""); 
+                          setPassword("");
+                          setConfirmPassword("");
+                        }
+                      } className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</a>
                   </p>
                 </div>:
                 <div className="py-2 px-4 pb-4 text-center">
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                       Don’t have an account yet? 
-                      <a href="#" onClick={()=>{setAction("Sign Up")}} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                      <a href="#" onClick={()=>{
+                            setAction("Sign Up"); 
+                            setAuthorized(-1);
+                            setUsername(""); 
+                            setPassword("");
+                            setConfirmPassword("");
+                          }
+                        } 
+                      className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                   </p>
                 </div>
             }
